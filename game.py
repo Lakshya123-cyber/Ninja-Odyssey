c1723683a7ce880732aa4a019540a32d77d7abda
Lakshya123-cyber <lakshyaraikwal85@gmail.com>
2024-02-25 13:44:21 +0200

    finished

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
 import math
+import os
 import random
 import pygame
 import sys
-from scripts.entities import PhysicsEntity, Player
+from scripts.entities import PhysicsEntity, Player, Enemy
+from scripts.spark import Spark
 from scripts.utils import load_image, load_images, Animation
 from scripts.tilemap import Tilemap
 from scripts.clouds import Clouds
@@ -15,11 +17,26 @@ class Game:
 
         pygame.display.set_caption("Ninja Odyssey")
         self.screen = pygame.display.set_mode((640, 480))
-        self.display = pygame.Surface((320, 240))
+
+        self.display = pygame.Surface((320, 240), pygame.SRCALPHA)
+        self.display_2 = pygame.Surface((320, 240))
 
         self.clock = pygame.time.Clock()
 
         self.movement = [False, False]
+
+        self.blue = (0, 200, 255)
+        self.orange = (255, 102, 0)
+
+        self.background_color = self.blue
+        self.target_color = self.orange
+
+        self.font = pygame.font.Font("data/Perfect DOS VGA 437.ttf", 72)
+        self.font2 = pygame.font.Font("data/PressStart2P.ttf", 25)
+        self.font3 = pygame.font.Font("data/Pic0 1.110.ttf", 40)
+
+        self.transition_duration = 2 * 60 * 1000
+        self.transition_start_time = pygame.time.get_ticks()
 
         self.assets = {
             "decor": load_images("tiles/decor"),
@@ -27,8 +44,9 @@ class Game:
             "large_decor": load_images("tiles/large_decor"),
             "stone": load_images("tiles/stone"),
             "player": load_image("entities/player.png"),
-            "background": load_image("background.png"),
             "clouds": load_images("clouds"),
+            "enemy/idle": Animation(load_images("entities/enemy/idle"), img_dur=6),
+            "enemy/run": Animation(load_images("entities/enemy/run"), img_dur=4),
             "player/idle": Animation(load_images("entities/player/idle"), img_dur=6),
             "player/run": Animation(load_images("entities/player/run"), img_dur=4),
             "player/jump": Animation(load_images("entities/player/jump")),
@@ -37,14 +55,153 @@ class Game:
             "particle/leaf": Animation(
                 load_images("particles/leaf"), img_dur=20, loop=False
             ),
+            "particle/particle": Animation(
+                load_images("particles/particle"), img_dur=6, loop=False
+            ),
+            "gun": load_image("gun.png"),
+            "projectile": load_image("projectile.png"),
         }
+
+        self.sfx = {
+            "jump": pygame.mixer.Sound("data/sfx/jump.wav"),
+            "ambience": pygame.mixer.Sound("data/sfx/ambience.wav"),
+            "dash": pygame.mixer.Sound("data/sfx/dash.wav"),
+            "hit": pygame.mixer.Sound("data/sfx/hit.wav"),
+            "shoot": pygame.mixer.Sound("data/sfx/shoot.wav"),
+        }
+
+        self.sfx["ambience"].set_volume(0.2)
+        self.sfx["shoot"].set_volume(0.4)
+        self.sfx["hit"].set_volume(0.8)
+        self.sfx["dash"].set_volume(0.3)
+        self.sfx["jump"].set_volume(0.7)
 
         self.clouds = Clouds(self.assets["clouds"], count=16)
 
         self.player = Player(self, (50, 50), (8, 15))
 
         self.tilemap = Tilemap(self, tile_size=16)
-        self.tilemap.load("map.json")
+
+        self.level = 0
+        self.load_level(self.level)
+
+        self.screen_shake = 0
+
+    def display_text(self, text, color, x, y):
+        outline_color = (0, 0, 0)  # Black outline
+        outline_surface = self.font.render(text, True, outline_color)
+
+        text_surface = self.font.render(text, True, color)
+        text_rect = text_surface.get_rect(center=(x, y))
+
+        for dx, dy in [(-1, -1), (-1, 1), (1, -1), (1, 1)]:
+            outline_rect = text_rect.move(dx, dy)
+            self.screen.blit(outline_surface, outline_rect)
+
+        self.screen.blit(text_surface, text_rect)
+
+    def display_text2(self, text, color, x, y):
+        outline_color = (0, 0, 0)  # Black outline
+        outline_surface = self.font2.render(text, True, outline_color)
+
+        text_surface = self.font2.render(text, True, color)
+        text_rect = text_surface.get_rect(center=(x, y))
+
+        for dx, dy in [(-1, -1), (-1, 1), (1, -1), (1, 1)]:
+            outline_rect = text_rect.move(dx, dy)
+            self.screen.blit(outline_surface, outline_rect)
+
+        self.screen.blit(text_surface, text_rect)
+
+    def display_text3(self, text, color, x, y):
+        outline_color = (0, 0, 0)  # Black outline
+        outline_surface = self.font3.render(text, True, outline_color)
+
+        text_surface = self.font3.render(text, True, color)
+        text_rect = text_surface.get_rect(center=(x, y))
+
+        for dx, dy in [(-1, -1), (-1, 1), (1, -1), (1, 1)]:
+            outline_rect = text_rect.move(dx, dy)
+            self.screen.blit(outline_surface, outline_rect)
+
+        self.screen.blit(text_surface, text_rect)
+
+    def main_menu(self):
+        pygame.mixer.music.load("data/music.wav")
+        pygame.mixer.music.set_volume(0.5)
+        pygame.mixer.music.play(-1)
+
+        self.sfx["ambience"].play(-1)
+        running = True
+        while running:
+
+            current_time = pygame.time.get_ticks()
+            elapsed_time = current_time - self.transition_start_time
+            progress = min(elapsed_time / self.transition_duration, 1.0)
+
+            if progress >= 1.0:
+                self.background_color, self.target_color = (
+                    self.target_color,
+                    self.background_color,
+                )
+                self.transition_start_time = current_time
+
+            interpolated_color = (
+                int(
+                    self.lerp(self.background_color[0], self.target_color[0], progress)
+                ),
+                int(
+                    self.lerp(self.background_color[1], self.target_color[1], progress)
+                ),
+                int(
+                    self.lerp(self.background_color[2], self.target_color[2], progress)
+                ),
+            )
+            self.screen.fill(interpolated_color)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        running = False
+                    if event.key == pygame.K_h:
+                        self.help_screen()
+
+            self.display_text("NINJA ODYSSEY", (255, 255, 255), 320, 185)
+            self.display_text2("PRESS ENTER TO PLAY", (255, 255, 255), 320, 260)
+            self.display_text2("PRESS H FOR HELP", (255, 255, 255), 320, 300)
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+    def help_screen(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if (
+                        event.key == pygame.K_ESCAPE
+                    ):  # Exit the help screen when Enter is pressed
+                        running = False
+
+            # Display the instructions
+            self.display_text("Instructions:", (255, 255, 255), 320, 100)
+            self.display_text3("Move left: A", (255, 255, 255), 320, 170)
+            self.display_text3("Move right: D", (255, 255, 255), 320, 220)
+            self.display_text3("Jump: Space", (255, 255, 255), 320, 270)
+            self.display_text3("Dash: L", (255, 255, 255), 320, 320)
+            self.display_text3("Press Esc to go back", (255, 255, 255), 320, 430)
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+    def load_level(self, map_id):
+        self.tilemap.load("data/maps/" + str(map_id) + ".json")
 
         self.leaf_spawners = []
         for tree in self.tilemap.extract([("large_decor", 2)], keep=True):
@@ -52,13 +209,73 @@ class Game:
                 pygame.Rect(4 + tree["pos"][0], 4 + tree["pos"][1], 23, 13)
             )
 
+        self.enemies = []
+
+        for spawner in self.tilemap.extract([("spawners", 0), ("spawners", 1)]):
+            if spawner["variant"] == 0:
+                self.player.pos = spawner["pos"]
+                self.player.air_time = 0
+            else:
+                self.enemies.append(Enemy(self, spawner["pos"], (8, 15)))
+
+        self.projectiles = []
         self.particles = []
+        self.sparks = []
 
         self.scroll = [0, 0]
+        self.dead = 0
+        self.transition = -30
+
+    def lerp(self, start, end, progress):
+        return start + progress * (end - start)
 
     def run(self):
+
+        self.sfx["ambience"].play(-1)
+
         while True:
-            self.display.blit(self.assets["background"], (0, 0))
+            current_time = pygame.time.get_ticks()
+            elapsed_time = current_time - self.transition_start_time
+            progress = min(elapsed_time / self.transition_duration, 1.0)
+
+            if progress >= 1.0:
+                self.background_color, self.target_color = (
+                    self.target_color,
+                    self.background_color,
+                )
+                self.transition_start_time = current_time
+
+            interpolated_color = (
+                int(
+                    self.lerp(self.background_color[0], self.target_color[0], progress)
+                ),
+                int(
+                    self.lerp(self.background_color[1], self.target_color[1], progress)
+                ),
+                int(
+                    self.lerp(self.background_color[2], self.target_color[2], progress)
+                ),
+            )
+
+            self.display.fill((0, 0, 0, 0))
+            self.display_2.fill(interpolated_color)
+
+            self.screen_shake = max(0, self.screen_shake - 1)
+
+            if not len(self.enemies):
+                self.transition += 1
+                if self.transition > 30:
+                    self.level = min(self.level + 1, len(os.listdir("data/maps")) - 1)
+                    self.load_level(self.level)
+            if self.transition < 0:
+                self.transition += 1
+
+            if self.dead:
+                self.dead += 1
+                if self.dead >= 10:
+                    self.transition += min(30, self.transition + 1)
+                if self.dead > 40:
+                    self.load_level(self.level)
 
             self.scroll[0] += (
                 self.player.rect().centerx
@@ -90,12 +307,96 @@ class Game:
                     )
 
             self.clouds.update()
-            self.clouds.render(self.display, offset=render_scroll)
+            self.clouds.render(self.display_2, offset=render_scroll)
 
             self.tilemap.render(self.display, offset=render_scroll)
 
-            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
-            self.player.render(self.display, offset=render_scroll)
+            for enemy in self.enemies.copy():
+                kill = enemy.update(self.tilemap, (0, 0))
+                enemy.render(self.display, offset=render_scroll)
+                if kill:
+                    self.enemies.remove(enemy)
+
+            if not self.dead:
+                self.player.update(
+                    self.tilemap, (self.movement[1] - self.movement[0], 0)
+                )
+                self.player.render(self.display, offset=render_scroll)
+
+            for projectile in self.projectiles.copy():
+                projectile[0][0] += projectile[1]
+                projectile[2] += 1
+                img = self.assets["projectile"]
+                self.display.blit(
+                    img,
+                    (
+                        projectile[0][0] - img.get_width() / 2 - render_scroll[0],
+                        projectile[0][1] - img.get_height() / 2 - render_scroll[1],
+                    ),
+                )
+
+                if self.tilemap.solid_check(projectile[0]):
+                    self.projectiles.remove(projectile)
+                    for i in range(4):
+                        self.sparks.append(
+                            Spark(
+                                projectile[0],
+                                random.random()
+                                - 0.5
+                                + (math.pi if projectile[1] > 0 else 0),
+                                2 + random.random(),
+                            )
+                        )
+                elif projectile[2] > 360:
+                    self.projectiles.remove(projectile)
+                elif abs(self.player.dashing) < 50:
+                    if self.player.rect().collidepoint(projectile[0]):
+                        self.projectiles.remove(projectile)
+                        self.dead += 1
+                        self.sfx["hit"].play()
+                        self.screen_shake = max(16, self.screen_shake)
+                        for i in range(30):
+                            angle = random.random() * math.pi * 2
+                            speed = random.random() * 5
+                            self.sparks.append(
+                                Spark(
+                                    self.player.rect().center,
+                                    angle,
+                                    2 + random.random(),
+                                )
+                            )
+                            self.particles.append(
+                                Particle(
+                                    self,
+                                    "particle",
+                                    self.player.rect().center,
+                                    velocity=[
+                                        math.cos(angle + math.pi) * speed * 0.5,
+                                        math.sin(angle + math.pi) * speed * 0.5,
+                                    ],
+                                    frame=random.randint(0, 7),
+                                )
+                            )
+
+            for spark in self.sparks.copy():
+                kill = spark.update()
+                spark.render(self.display, offset=render_scroll)
+                if kill:
+                    self.sparks.remove(spark)
+
+            display_mask = pygame.mask.from_surface(self.display)
+            display_sillhouette = display_mask.to_surface(
+                setcolor=(0, 0, 0, 180),
+                unsetcolor=(
+                    0,
+                    0,
+                    0,
+                    0,
+                ),
+            )
+
+            for offset in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                self.display_2.blit(display_sillhouette, offset)
 
             for particle in self.particles.copy():
                 kill = particle.update()
@@ -118,7 +419,10 @@ class Game:
                     if event.key == pygame.K_d:
                         self.movement[1] = True
                     if event.key == pygame.K_SPACE:
-                        self.player.jump()
+                        if self.player.jump():
+                            self.sfx["jump"].play()
+                    if event.key == pygame.K_l:
+                        self.player.dash()
 
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_a:
@@ -126,8 +430,27 @@ class Game:
                     if event.key == pygame.K_d:
                         self.movement[1] = False
 
+            if self.transition:
+                transition_surf = pygame.Surface(self.display.get_size())
+                pygame.draw.circle(
+                    transition_surf,
+                    (255, 255, 255),
+                    (self.display.get_width() // 2, self.display.get_height() // 2),
+                    (30 - abs(self.transition)) * 8,
+                )
+                transition_surf.set_colorkey((255, 255, 255))
+                self.display.blit(transition_surf, (0, 0))
+
+            self.display_2.blit(self.display, (0, 0))
+
+            screenshake_offset = (
+                random.random() * self.screen_shake - self.screen_shake / 2,
+                random.random() * self.screen_shake - self.screen_shake / 2,
+            )
+
             self.screen.blit(
-                pygame.transform.scale(self.display, self.screen.get_size()), (0, 0)
+                pygame.transform.scale(self.display_2, self.screen.get_size()),
+                screenshake_offset,
             )
 
             pygame.display.update()
@@ -135,5 +458,5 @@ class Game:
 
 
 game = Game()
-
+game.main_menu()
 game.run()
